16347795433ffad0dd72ede50fd740bb5d655827
Phanuwat2828 <131578554+Phanuwat2828@users.noreply.github.com>
2024-03-12 18:36:01 +0700

    update

--- Ch7-Ex/Ex_2.java
+++ Ch7-Ex/Ex_2.java
@@ -3,20 +3,21 @@
 import java.util.Scanner;
 
 public class Ex_2 {
-    String change_data(String data){
+    String change_data(String data) {
         String change = new StringBuilder(data).reverse().toString();
         return change;
     }
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Ex_2 ob = new Ex_2();
         System.out.print("Enter : ");
         String data = input.nextLine();
         String data_change = ob.change_data(data);
-        if(data_change.equals(data)){
-            System.out.print(data+" Is Parindom");
-        }else{
-            System.out.print(data+" Is not Parindom");
+        if (data_change.equals(data)) {
+            System.out.print(data + " Is Parindom");
+        } else {
+            System.out.print(data + " Is not Parindom");
         }
     }
 }
