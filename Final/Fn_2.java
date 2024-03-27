c93936891f0f558812cc7edb4e31f743a632f5eb
Phanuwat2828 <131578554+Phanuwat2828@users.noreply.github.com>
2024-03-27 20:17:39 +0700

    fn_4

---
+++ Final/Fn_2.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Fn_2 {
+    HashMap data_animal(String data) {
+        StringTokenizer data_token = new StringTokenizer(data);
+        HashMap<String,Integer> data_return = new HashMap<String,Integer>();
+        for (; data_token.hasMoreTokens();) {
+            if(data_token.nextToken())
+        }
+        return data_return;
+    }
+
+    public static void main(String[] args) {
+        Fn_2 ob = new Fn_2();
+        System.out.print("Enter : ");
+        Scanner input = new Scanner(System.in);
+        String data = input.nextLine();
+    }
+}
--- final_java/Fn_4.java
+++ final_java/Fn_4.java
@@ -3,46 +3,72 @@ import java.util.Scanner;
 import java.io.File;
 import java.util.StringTokenizer;
 import java.util.HashMap;
+
 public class Fn_4 {
-    int posees(String[] num ,char operator,HashMap<String,String> dataMap){
-        for(int i=0;i<num.length;i++){
+    int posees(String[] num, char operator, HashMap<String, String> dataMap) {
+        for (int i = 0; i < num.length; i++) {
             StringTokenizer data_number = new StringTokenizer(num[i]);
             StringBuffer word = new StringBuffer();
             while (data_number.hasMoreTokens()) {
                 String data_next = data_number.nextToken().toLowerCase();
                 word.append(dataMap.get(data_next));
             }
-            System.out.print(word);
+            num[i] = word.toString();
         }
-        return 0;
+        int sum = 0;
+        if (operator == '+') {
+            sum = Integer.parseInt(num[0]) + Integer.parseInt(num[1]);
+        } else if (operator == '-') {
+            sum = Integer.parseInt(num[0]) - Integer.parseInt(num[1]);
+        } else if (operator == '*') {
+            sum = Integer.parseInt(num[0]) * Integer.parseInt(num[1]);
+        } else if (operator == '/') {
+            sum = Integer.parseInt(num[0]) / Integer.parseInt(num[1]);
+        } else if (operator == '%') {
+            sum = Integer.parseInt(num[0]) % Integer.parseInt(num[1]);
+        }
+        System.out.print(num[0] + operator + num[1] + "= " + sum + "\n");
+        return sum;
     }
-    public static void main(String[] args){
-        try{
+
+    void change(int number, HashMap<String, String> data_Map) {
+        String data = String.valueOf(number);
+        for (int i = 0; i < data.length(); i++) {
+            for (String key : data_Map.keySet()) {
+                if (data_Map.get(key).equals(String.valueOf(data.charAt(i)))) {
+                    System.out.print(key + " ");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
             Scanner input_str = new Scanner(System.in);
             Scanner input_char = new Scanner(System.in);
             Fn_4 ob = new Fn_4();
             String num[] = new String[2];
-            HashMap<String,String> digitmap = new HashMap<String,String>();
-            digitmap.put("zero","0");
-            digitmap.put("one","1");
-            digitmap.put("two","2");
-            digitmap.put("three","3");
-            digitmap.put("four","4");
-            digitmap.put("five","5");
-            digitmap.put("six","6");
-            digitmap.put("seven","7");
-            digitmap.put("eight","8");
-            digitmap.put("nine","9");
+            HashMap<String, String> digitmap = new HashMap<String, String>();
+            digitmap.put("zero", "0");
+            digitmap.put("one", "1");
+            digitmap.put("two", "2");
+            digitmap.put("three", "3");
+            digitmap.put("four", "4");
+            digitmap.put("five", "5");
+            digitmap.put("six", "6");
+            digitmap.put("seven", "7");
+            digitmap.put("eight", "8");
+            digitmap.put("nine", "9");
             char operator;
             System.out.print("Enter number[1] : ");
-            num[1] = input_str.nextLine();
+            num[0] = input_str.nextLine();
             System.out.print("Enter Oprator : ");
             operator = input_char.next().charAt(0);
             System.out.print("Enter number[2] : ");
             num[1] = input_str.nextLine();
-            ob.posees(num, operator, digitmap);
-        }catch(Exception e){
-            System.out.print(e);
+            int test = ob.posees(num, operator, digitmap);
+            ob.change(test, digitmap);
+        } catch (Exception e) {
         }
     }
 }
